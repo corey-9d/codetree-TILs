875e144cd3d1b50bcc4d3c7c83037774751535e2
corey-9d <corey9d@gmail.com>
2024-09-26 04:16:08 +0900

    Title: 4번의 크기 비교, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240926/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,12 @@
+a = int(input())
+inp = input().split(" ")
+b = int(inp[0])
+c = int(inp[1])
+d = int(inp[2])
+e = int(inp[3])
+
+
+print("1" if a > b else "0")
+print("1" if a > c else "0")
+print("1" if a > d else "0")
+print("1" if a > e else "0")
