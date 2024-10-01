a169e58fb756da231e1d3d972c43aa003fda4c7f
corey-9d <corey9d@gmail.com>
2024-10-02 06:05:35 +0900

    Title: 높은 수에서 낮은 수까지, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 241002/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,9 +1,9 @@
 a,b = map(int, input().split())
 
-if a < b:
+if a <= b:
     for i in range(b, a - 1, -1):
         print(i, end=" ")
 
-elif b < a:
+elif b <= a:
     for i in range(a, b - 1, -1):
         print(i, end=" ")
